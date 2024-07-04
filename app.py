67d3b700b1f00345679ceb0fb50db9382e4373ba
Agwam AA Khomaeini <135943738+away1205@users.noreply.github.com>
2024-07-04 22:50:04 +0800

    change the endpoint app.py

--- app.py
+++ app.py
@@ -48,17 +48,17 @@ api = Api(app)
 
 # Set endpoints to get the data
 api.add_resource(Users, "/api/users/")
-api.add_resource(User, "/api/users/<int:id>")
+api.add_resource(User, "/api/users/<string:id>")
 api.add_resource(Products, "/api/products/")
-api.add_resource(ProductResource, "/api/products/<int:id>")
+api.add_resource(ProductResource, "/api/products/<string:id>")
 api.add_resource(CategoryProducts, "/api/categories/")
-api.add_resource(CategoryProductResource, "/api/categories/<int:id>")
+api.add_resource(CategoryProductResource, "/api/categories/<string:id>")
 api.add_resource(DetailTransactions, "/api/detail-transactions/")
-api.add_resource(DetailTransactionResource, "/api/detail-transactions/<int:id>")
+api.add_resource(DetailTransactionResource, "/api/detail-transactions/<string:id>")
 api.add_resource(Transactions, "/api/transactions/")
-api.add_resource(TransactionResource, "/api/transactions/<int:id>")
+api.add_resource(TransactionResource, "/api/transactions/<string:id>")
 api.add_resource(Restocks, "/api/restocks/")
-api.add_resource(RestockResource, "/api/restocks/<int:id>")
+api.add_resource(RestockResource, "/api/restocks/<string:id>")
 
 
 @app.route("/")
